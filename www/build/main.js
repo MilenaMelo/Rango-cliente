cb98766b3a29539977314b35f0f5b005eb2c42a8
Milena Melo <milenasmelo@outlook.com>
2018-12-07 14:43:03 -0300

    TelaBusca

--- www/build/main.js
+++ www/build/main.js
@@ -4,6 +4,47 @@ webpackJsonp([2],{
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return LoginPage; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+var LoginPage = /** @class */ (function () {
+    function LoginPage() {
+    }
+    LoginPage.prototype.ionViewDidLoad = function () {
+        console.log('ionViewDidLoad LoginPage');
+    };
+    LoginPage = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'page-login',template:/*ion-inline-start:"C:\Users\User\Documents\Ionic\projetoApp\AppRango\src\pages\login\login.html"*/'\n<link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700" rel="stylesheet">\n\n<ion-header>\n    <ion-navbar>\n        <img class="img-back" src="assets/imgs/back.png">      \n        <div class="brandName">RanGo</div>\n        <button ion-button menuToggle>\n            <ion-icon name="menu"></ion-icon>\n          </button>\n    </ion-navbar>\n</ion-header>\n\n<ion-content padding class="padding">\n\n    <div class="list card"> \n        <div class="item-body">\n          <img class="full-image" src="assets/imgs/usuario.png">\n        </div>\n    </div>  \n\n    <div class="container-inputs">\n\n        <ion-list class="inputList">  \n          <ion-item>\n            <ion-input type="email" placeholder="Email"></ion-input>\n          </ion-item>\n          <div class="brandName-login">Esqueceu a senha?</div>\n            <ion-item>\n              <ion-input type="password" placeholder="Senha"></ion-input>\n            </ion-item>     \n        </ion-list>\n          <button class="button button-block button-assertive">\n              Entrar\n          </button>\n          <div class="brandName-login-2">Não tem uma conta? Cadastre-se AQUI > CADASTRAR</div>  \n    </div>\n</ion-content>\n'/*ion-inline-end:"C:\Users\User\Documents\Ionic\projetoApp\AppRango\src\pages\login\login.html"*/,
+        }),
+        __metadata("design:paramtypes", [])
+    ], LoginPage);
+    return LoginPage;
+}());
+
+//# sourceMappingURL=login.js.map
+
+/***/ }),
+
+/***/ 101:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return BuscaPage; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__ionic_native_geolocation__ = __webpack_require__(153);
@@ -59,47 +100,6 @@ var BuscaPage = /** @class */ (function () {
 
 /***/ }),
 
-/***/ 101:
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return LoginPage; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-var LoginPage = /** @class */ (function () {
-    function LoginPage() {
-    }
-    LoginPage.prototype.ionViewDidLoad = function () {
-        console.log('ionViewDidLoad LoginPage');
-    };
-    LoginPage = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-login',template:/*ion-inline-start:"C:\Users\User\Documents\Ionic\projetoApp\AppRango\src\pages\login\login.html"*/'\n<link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700" rel="stylesheet">\n\n<ion-header>\n    <ion-navbar>\n        <img class="img-back" src="assets/imgs/back.png">      \n        <div class="brandName">RanGo</div>\n        <button ion-button menuToggle>\n            <ion-icon name="menu"></ion-icon>\n          </button>\n    </ion-navbar>\n</ion-header>\n\n<ion-content padding class="padding">\n\n    <div class="list card"> \n        <div class="item-body">\n          <img class="full-image" src="assets/imgs/usuario.png">\n        </div>\n    </div>  \n\n    <div class="container-inputs">\n\n        <ion-list class="inputList">  \n          <ion-item>\n            <ion-input type="email" placeholder="Email"></ion-input>\n          </ion-item>\n          <div class="brandName-login">Esqueceu a senha?</div>\n            <ion-item>\n              <ion-input type="password" placeholder="Senha"></ion-input>\n            </ion-item>     \n        </ion-list>\n          <button class="button button-block button-assertive">\n              Entrar\n          </button>\n          <div class="brandName-login-2">Não tem uma conta? Cadastre-se AQUI > CADASTRAR</div>  \n    </div>\n</ion-content>\n'/*ion-inline-end:"C:\Users\User\Documents\Ionic\projetoApp\AppRango\src\pages\login\login.html"*/,
-        }),
-        __metadata("design:paramtypes", [])
-    ], LoginPage);
-    return LoginPage;
-}());
-
-//# sourceMappingURL=login.js.map
-
-/***/ }),
-
 /***/ 111:
 /***/ (function(module, exports) {
 
@@ -122,11 +122,11 @@ webpackEmptyAsyncContext.id = 111;
 
 var map = {
 	"../pages/busca/busca.module": [
-		272,
+		273,
 		1
 	],
 	"../pages/login/login.module": [
-		273,
+		272,
 		0
 	]
 };
@@ -170,7 +170,7 @@ var HomePage = /** @class */ (function () {
     }
     HomePage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-home',template:/*ion-inline-start:"C:\Users\User\Documents\Ionic\projetoApp\AppRango\src\pages\home\home.html"*/'<link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700" rel="stylesheet">\n\n<ion-header>\n    <ion-navbar>\n        <img class="img-back" src="assets/imgs/back.png">      \n        <div class="brandName">RanGo</div>\n        <button ion-button menuToggle>\n            <ion-icon name="menu"></ion-icon>\n          </button>\n    </ion-navbar>\n</ion-header>\n\n<ion-content padding class="padding">\n\n    <div class="list card">\n      \n        <div class="item-body">\n          <img class="full-image" src="assets/imgs/usuario.png">\n        </div>\n\n    </div>  \n    <div class="container-inputs">\n          <button class="button button-block button-light" >\n            <img class="img-gmail" src="assets/imgs/gmail.png" alt="gmail">\n            <div class="title-button"> Cadastrar com o GOOGLE </div>\n          </button>\n  \n          <ion-list class="inputList">\n            <ion-item>\n              <ion-input type="text" placeholder="Nome"></ion-input>\n            </ion-item>\n            \n            <ion-item>\n              <ion-input type="email" placeholder="Email"></ion-input>\n            </ion-item>\n            \n              <ion-item>\n                <ion-input type="password" placeholder="Senha"></ion-input>\n              </ion-item>\n            \n              <ion-item>\n                <ion-input type="password" placeholder="Confirmar Senha"></ion-input>\n              </ion-item> \n          </ion-list>\n          \n          <button class="button button-block button-assertive">\n              Cadastrar\n          </button>\n      </div>\n       \n</ion-content>\n\n'/*ion-inline-end:"C:\Users\User\Documents\Ionic\projetoApp\AppRango\src\pages\home\home.html"*/
+            selector: 'page-home',template:/*ion-inline-start:"C:\Users\User\Documents\Ionic\projetoApp\AppRango\src\pages\home\home.html"*/'<link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700" rel="stylesheet">\n\n<ion-header>\n    <ion-navbar>\n        <img class="img-back" src="assets/imgs/back.png">      \n        <div class="brandName">RanGo</div>\n        <button ion-button menuToggle>\n            <ion-icon name="menu"></ion-icon>\n          </button>\n    </ion-navbar>\n</ion-header>\n\n<ion-content padding class="padding">\n\n    <div class="list card">\n      \n        <div class="item-body">\n          <img class="full-image" src="assets/imgs/usuario.png">\n        </div>\n\n    </div>  \n    <div class="container-inputs">\n          <button class="button button-block button-light" >\n            <img class="img-gmail" src="assets/imgs/gmail.png" alt="gmail">\n            <div class="title-button"> Cadastrar com o GOOGLE </div>\n          </button>\n  \n          <ion-list class="inputList">\n            <ion-item>\n              <ion-input type="text" placeholder="Nome"></ion-input>\n            </ion-item>\n            \n            <ion-item>\n              <ion-input type="text" placeholder="CPF"></ion-input>\n            </ion-item>\n\n            <ion-item>\n              <ion-input type="email" placeholder="Email"></ion-input>\n            </ion-item>\n            \n              <ion-item>\n                <ion-input type="password" placeholder="Senha"></ion-input>\n              </ion-item>\n            \n              <ion-item>\n                <ion-input type="password" placeholder="Confirmar Senha"></ion-input>\n              </ion-item> \n          </ion-list>\n          \n          <button class="button button-block button-assertive">\n              Cadastrar\n          </button>\n      </div>\n       \n</ion-content>\n\n'/*ion-inline-end:"C:\Users\User\Documents\Ionic\projetoApp\AppRango\src\pages\home\home.html"*/
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */]])
     ], HomePage);
@@ -265,8 +265,8 @@ Object(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* pl
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__pages_list_list__ = __webpack_require__(197);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__ionic_native_status_bar__ = __webpack_require__(194);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__ionic_native_splash_screen__ = __webpack_require__(195);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__pages_login_login__ = __webpack_require__(101);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__pages_busca_busca__ = __webpack_require__(100);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__pages_login_login__ = __webpack_require__(100);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__pages_busca_busca__ = __webpack_require__(101);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__ionic_native_geolocation__ = __webpack_require__(153);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -301,8 +301,8 @@ var AppModule = /** @class */ (function () {
                 __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
                 __WEBPACK_IMPORTED_MODULE_2_ionic_angular__["c" /* IonicModule */].forRoot(__WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* MyApp */], {}, {
                     links: [
-                        { loadChildren: '../pages/busca/busca.module#BuscaPageModule', name: 'BuscaPage', segment: 'busca', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/login/login.module#LoginPageModule', name: 'LoginPage', segment: 'login', priority: 'low', defaultHistory: [] }
+                        { loadChildren: '../pages/login/login.module#LoginPageModule', name: 'LoginPage', segment: 'login', priority: 'low', defaultHistory: [] },
+                        { loadChildren: '../pages/busca/busca.module#BuscaPageModule', name: 'BuscaPage', segment: 'busca', priority: 'low', defaultHistory: [] }
                     ]
                 }),
             ],
@@ -340,8 +340,8 @@ var AppModule = /** @class */ (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(195);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pages_home_home__ = __webpack_require__(196);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__pages_list_list__ = __webpack_require__(197);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_login_login__ = __webpack_require__(101);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__pages_busca_busca__ = __webpack_require__(100);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_login_login__ = __webpack_require__(100);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__pages_busca_busca__ = __webpack_require__(101);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
